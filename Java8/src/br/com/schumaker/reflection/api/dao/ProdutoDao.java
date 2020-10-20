99c8f8dc50f3298d494a8401e3ba9f0e36c254d6
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-10-20 20:22:08 +0200

    Update ProdutoDao.java

--- Java8/src/br/com/schumaker/reflection/api/dao/ProdutoDao.java
+++ Java8/src/br/com/schumaker/reflection/api/dao/ProdutoDao.java
@@ -3,6 +3,10 @@ package br.com.schumaker.reflection.api.dao;
 import br.com.schumaker.reflection.api.modelo.Produto;
 import java.util.List;
 
+/**
+ * @author Hudson Schumaker
+ * @version 1.0.0
+ */
 public interface ProdutoDao {
 
     public List<Produto> lista();
